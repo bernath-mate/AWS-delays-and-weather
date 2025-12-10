64bee15bba532953caeccdbc18aea7a8fd598ee8
Bernáth Máté Levente <bernath.mate@outlook.com>
2025-12-10 14:02:10 +0100

    Synchronizing job unified_all-weekly-update-ETL from AWS Glue at 2025/12/10 13:02:10

--- unified_all-weekly-update-ETL/unified_all-weekly-update-ETL.py
+++ unified_all-weekly-update-ETL/unified_all-weekly-update-ETL.py
@@ -20,7 +20,7 @@ athena_client = boto3.client('athena', region_name='us-east-1')
 try:
     print("starting unified_all view refresh ETL job")
     
-    # Create or replace unified_all view
+    # create or replace unified_all view
     try:
         print("refreshing unified_all view")
         
@@ -29,23 +29,21 @@ try:
         SELECT 
             d.date,
             d.station_name,
-            sr.region,
             sr.county,
+            sr.region,
             d.total_delay_minutes,
             d.train_count,
             d.avg_delay_minutes,
-            w.temperature_mean_c,
-            w.wind_gust_max_kmh,
-            w.precipitation_sum_mm,
-            w.extreme_temperature,
-            w.extreme_wind,
-            w.extreme_precipitation
+            w.high_temp,
+            w.high_wind,
+            w.high_precip
         FROM delays_all d
         LEFT JOIN stations_regions sr 
-            ON d.station_name = sr.station_name
+            ON d.station_id = sr.station_id
         LEFT JOIN weather_all w 
             ON d.date = w.date 
-            AND sr.region = w.station_name
+           AND sr.region_id = w.region_id
+        WHERE d.station_id IS NOT NULL
         ORDER BY d.date, d.station_name;
         """
         
